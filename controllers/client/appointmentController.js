9ce32be1693918aeadd5647036a6e33c3e3584a0
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2024-01-22 22:32:59 +0900

    fix: apppintments have been reconfigured

--- controllers/client/appointmentController.js
+++ controllers/client/appointmentController.js
@@ -22,43 +22,61 @@ appointmentController.getAllAppointments = async (req, res) => {
 
 appointmentController.getChosenAppointment = async (req, res) => {
   try {
-    console.log("GET: client/getChosenAppointment ");
-    const { ref_id } = req.params;
-    const { date } = req.query;
-    const foundDoctor = await memberModel.findById(ref_id);
-
-    if (!foundDoctor)
-      return res.status(404).json({ message: "Doctor not found" });
-
-    const foundAppointment = await appointmentModel.findOne({
-      date,
-      doctor_id: ref_id,
-    });
-
-    if (!foundAppointment) {
-      const createData = await appointmentModel.create({
-        date,
-        doctor_id: foundDoctor._id,
-      });
-
-      return res
-        .status(200)
-        .json({ message: "Appointment created", slots: createData.slots });
-    }
-
-    res.json({ state: "success", slots: foundAppointment.slots });
+     console.log("GET: client/getChosenAppointment ");
+     const { ref_id } = req.params;
+     const { date } = req.query;
+     const foundDoctor = await memberModel.findById(ref_id);
+ 
+     if (!foundDoctor)
+       return res.status(404).json({ message: "Doctor not found" });
+ 
+     const foundAppointment = await appointmentModel.findOne({
+       date,
+       doctor_id: ref_id,
+     });
+ 
+     if (!foundAppointment) {
+       const createData = await appointmentModel.create({
+         date,
+         doctor_id: foundDoctor._id,
+       });
+ 
+       return res
+         .status(200)
+         .json({ message: "Appointment created", slots: createData.slots });
+     }
+ 
+     // Modify the slots array to match the new structure
+     const modifiedSlots = foundAppointment.slots.map((slot) => {
+       if (slot.ref_id) {
+         return {
+           _id: slot._id,
+           start: slot.start,
+           end: slot.end,
+           ref_id: slot.ref_id,
+         };
+       } else {
+         return {
+           _id: slot._id,
+           start: slot.start,
+           end: slot.end,
+         };
+       }
+     });
+ 
+     res.json({ state: "success", slots: modifiedSlots });
   } catch (err) {
-    console.log(`ERROR, client/getChosenAppointment, ${err.message}`);
-    res.json({ state: "fail", message: err.message });
+     console.log(`ERROR, client/getChosenAppointment, ${err.message}`);
+     res.json({ state: "fail", message: err.message });
   }
-};
+ };
 
 appointmentController.createAppointment = async (req, res) => {
   try {
     console.log("POST: client/createAppointment");
     const { ref_id } = req.params; // doctor_id
     const { date } = req.query;
-    const { slot_id, mem_id, start, end } = req.body; // Use start and end instead of slot_time
+    const { mem_id, start, end } = req.body; // Use start and end instead of slot_time
 
     console.log(`ref_id: ${ref_id}, date: ${date}`);
 
@@ -125,73 +143,72 @@ appointmentController.createAppointment = async (req, res) => {
 
 appointmentController.getAppointmentsForUser = async (req, res) => {
   try {
-    console.log("GET: client/getAppointmentsForUser");
-    const { id } = req.params;
-
-    console.log("id:", id);
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    // Get all appointments
-    const appointments = await appointmentModel.find().populate("doctor_id");
-    console.log("appointments", appointments);
-
-    const filteredAppointments = appointments.filter((appointment) => {
-      const appointmentDate = new Date(appointment.date);
-      return appointmentDate >= today;
-    });
-
-    const mappedAppointments = await Promise.all(
-      filteredAppointments.map(async (appointment) => {
-        // Only iterate over slots if appointment and appointment.slots are defined
-        if (appointment && appointment.slots) {
-          return {
-            ...appointment._doc,
-            slots: await Promise.all(
-              appointment.slots
-                .filter(
+     console.log("GET: client/getAppointmentsForUser");
+     const { id } = req.params;
+ 
+     console.log("id:", id);
+ 
+     const today = new Date();
+     today.setHours(0, 0, 0, 0);
+ 
+     // Get all appointments
+     const appointments = await appointmentModel.find().populate("doctor_id");
+     console.log("appointments", appointments);
+ 
+     const filteredAppointments = appointments.filter((appointment) => {
+       const appointmentDate = new Date(appointment.date);
+       return appointmentDate >= today;
+     });
+ 
+     const mappedAppointments = await Promise.all(
+       filteredAppointments.map(async (appointment) => {
+         // Only iterate over slots if appointment and appointment.slots are defined
+         if (appointment && appointment.slots) {
+           return {
+             ...appointment._doc,
+             slots: await Promise.all(
+               appointment.slots
+                 .filter(
                   (slotValue) =>
-                    slotValue.doctor_id && slotValue.start && slotValue.end
-                ) // filter slots that have appointments
-                .map(async (slotValue) => {
+                     slotValue.doctor_id && slotValue.start && slotValue.end
+                 ) // filter slots that have appointments
+                 .map(async (slotValue) => {
                   const doctor = await memberModel.findById(
-                    slotValue.doctor_id
+                     slotValue.doctor_id
                   );
                   return {
-                    ...slotValue,
-                    appointmentDate: `${appointment.date} - ${slotValue.start} : ${slotValue.end}`,
-                    doctorName: doctor.mb_name,
-                    doctorLastname: doctor.mb_last_name,
-                    doctorImg: doctor.mb_image,
+                     ...slotValue,
+                     appointmentDate: `${appointment.date} - ${slotValue.start} : ${slotValue.end}`,
+                     doctorName: doctor.mb_name,
+                     doctorLastname: doctor.mb_last_name,
+                     doctorImg: doctor.mb_image,
                   };
-                })
-            ),
-          };
-        } else {
-          return null;
-        }
-      })
-    );
-
-    console.log("mappedAppointments", mappedAppointments);
-
-    const finalAppointments = mappedAppointments.filter(
-      (appointment) => appointment !== null
-    );
-
-    console.log("mappeAppointments", mappedAppointments);
-
-    res.json({
-      state: "success",
-      appointment_data: finalAppointments, // return all appointments
-    });
+                 })
+             ),
+           };
+         } else {
+           return null;
+         }
+       })
+     );
+ 
+     console.log("mappedAppointments", mappedAppointments);
+ 
+     const finalAppointments = mappedAppointments.filter(
+       (appointment) => appointment !== null
+     );
+ 
+     console.log("mappeAppointments", mappedAppointments);
+ 
+     res.json({
+       state: "success",
+       appointment_data: finalAppointments, // return all appointments
+     });
   } catch (err) {
-    console.log(`ERROR, secued/getDoctorDashboard , ${err.message}`);
-    res.status(500).json({ message: err.message }); // send a response with the error message
+     console.log(`ERROR, secued/getDoctorDashboard , ${err.message}`);
+     res.status(500).json({ message: err.message }); // send a response with the error message
   }
-};
-
+ };
 
 appointmentController.updateAppointment = async (req, res) => {
   try {
