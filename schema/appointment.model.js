67c0ab06954de4d779ea68537be79c735e520110
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2024-01-29 13:07:10 +0900

    feat: changed  apointment schema for testing

--- schema/appointment.model.js
+++ schema/appointment.model.js
@@ -4,206 +4,125 @@ const slot = require("./slot.model");
 
 const default_slots = [
   {
-    _id: 1,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+    _id: 0,
     start: "08:00",
     end: "08:30",
   },
-
-  {_id: 2,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 1,
     start: "08:30",
     end: "09:00",
   },
 
-  {_id: 3,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 2,
     start: "09:00",
     end: "09:30",
   },
-
-  {_id: 4,  
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 3,
     start: "09:30",
     end: "10:00",
   },
 
-  {_id: 5,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 4,
     start: "10:00",
     end: "10:30",
   },
-
-  {_id: 6,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 5,
     start: "10:30",
     end: "11:00",
   },
 
-  {_id: 7,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 6,
     start: "11:00",
     end: "11:30",
   },
-
-  {_id: 8,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 7,
     start: "11:30",
     end: "12:00",
-  },  
+  },
 
-  {_id: 9,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 8,
     start: "12:00",
     end: "12:30",
   },
-
-  {_id: 10,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 9,
     start: "12:30",
     end: "13:00",
   },
 
-  {_id: 11,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 10,
     start: "13:00",
     end: "13:30",
   },
-
-  {_id: 12,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 11,
     start: "13:30",
     end: "14:00",
   },
 
-  {_id: 13,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 12,
     start: "14:00",
     end: "14:30",
   },
-
-  {_id: 14,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 13,
     start: "14:30",
     end: "15:00",
   },
 
-  {_id: 15,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 14,
     start: "15:00",
     end: "15:30",
   },
-
-  {_id: 16,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 15,
     start: "15:30",
     end: "16:00",
   },
 
-  {_id: 17,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 16,
     start: "16:00",
     end: "16:30",
   },
-
-  {_id: 18,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 17,
     start: "16:30",
     end: "17:00",
   },
 
-  {_id: 19,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 18,
     start: "17:00",
     end: "17:30",
   },
-
-  {_id: 20,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 19,
     start: "17:30",
     end: "18:00",
   },
 
-  {_id: 21,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 20,
     start: "18:00",
     end: "18:30",
   },
-
-  {_id: 22,
-    ref_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member'
-    },
+  {
+    _id: 21,
     start: "18:30",
     end: "19:00",
   },
-  
-
- 
 ];
 
 const appointmentSchema = new mongoose.Schema(
@@ -222,7 +141,6 @@ const appointmentSchema = new mongoose.Schema(
     doctor_id: {
       type: String,
       required: true,
-     
     },
   },
   { timestamps: true }
